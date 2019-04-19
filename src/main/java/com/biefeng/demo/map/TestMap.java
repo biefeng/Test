6c0b54e56dff524925c56d9437c823360e7c142d
335049213@qq.com <biefeng123>
2019-04-20 01:04:37 +0800

    test1

--- src/main/java/com/biefeng/demo/map/TestMap.java
+++ src/main/java/com/biefeng/demo/map/TestMap.java
@@ -2,18 +2,31 @@ package com.biefeng.demo.map;
 
 import org.junit.Test;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
 public class TestMap {
 
-    public static void main(String[] args) {
+    @Test
+    public void test1() {
+        SortedSet<String> sortedSet = new TreeSet<>();
+        for (int i = 0; i < 10; i++) {
+            sortedSet.add(Integer.toString(i));
+        }
+        SortedSet<String> newSet = ((TreeSet<String>) sortedSet).headSet("3");
 
-        Map<String,String> map  = new HashMap<>();
-        map.put("name","别峰");
-        System.out.println(map.get("name"));
+        for(String str:newSet){
+            System.out.println(str);
+        }
     }
 
+    @Test
+    public void name() {
 
 
+    }
+
+    public static void main(String[] args) {
+        Map<String,String> map = new HashMap<>();
+        map.put("name","111");
+    }
 }
